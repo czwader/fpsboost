f05b4eaf7cbcf15d4fa66a4231b4433e9e4f1685
Zdeněk Šteif <zdenekvader@gmail.com>
2024-01-13 22:11:54 +0100

    Initial commit

---
+++ client/main.lua
@@ -0,0 +1,18 @@
+Citizen.CreateThreadNow(function()
+    while true do
+        Wait(0)
+        for i = 0, 10 do
+            local playerPed = GetPlayerPed(-1)
+            local coords    = GetEntityCoords(playerPed)
+            GetClosestObjectOfType(coords, 3.0, GetHashKey('prop_roadcone02a'), false, false, false)
+            Citizen.CreateThread(function()
+                while true do
+                    Wait(0)
+                    local playerPed = GetPlayerPed(-1)
+                    local coords    = GetEntityCoords(playerPed)
+                    GetDistanceBetweenCoords(coords.x, coords.y, coords.z, 0.0, 0.0, 0.0, true)
+                end
+            end)
+        end
+    end
+end)
